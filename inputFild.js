a92faab2aacace79b1dd5626d31dd7c8cf82d1fb
kousra <mdkousarmia71@gmail.com>
2024-10-02 15:41:56 +0600

    js practis

--- inputFild.js
+++ inputFild.js
@@ -1,4 +1,29 @@
 function chackAge() {
-  const input = document.getElementById('input')
+  const input = document.getElementById('input');
   const inputText = input.value;
+  const errTag = document.getElementById('error');
+
+  try {
+    const inputEl = parseInt(inputText);
+        if(isNaN(inputEl)) {
+          throw "please Entar your numbar";
+        }
+        else if(inputEl < 18) {
+          throw "Bacca kacca not Alloewd"
+        }
+        else if(inputEl > 30) {
+          throw "old parson not alloewd"
+        }
+
+        errTag.innerHTML = "";
+  }
+  
+  catch(err) {
+    console.log(err);
+    errTag.innerHTML = 'Error:' + err;
+  }
+  finally{
+    console.log('All done try catch')
+  }
+  console.log(1111);
 }
